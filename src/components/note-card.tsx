5ed311fde50c6691945c0f8a4766f3c0eb4dab3d
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-03-27 16:51:51 -0400

    ✨ comit  init

---
+++ src/components/note-card.tsx
@@ -0,0 +1,11 @@
+export function NoteCard() {
+  return (
+    <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+      <span className='text-sm font-medium text-slate-300'>há 2 dias</span>
+      <p className='text-sm leading-6 text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo sint facere sapiente corporis adipisci eos cupiditate aliquid ipsa amet culpa, itaque quasi non placeat beatae debitis, aut molestias sequi accusamus.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo sint facere sapiente corporis adipisci eos cupiditate aliquid ipsa amet culpa, itaque quasi non placeat beatae debitis, aut molestias sequi accusamus.</p>
+
+      <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
+    </div>
+  )
+}
