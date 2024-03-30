b763d14e440996193fedaa6738692052a85987d0
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-03-30 16:33:11 -0400

    ✨ Criando as notas, salvando no storage e buscnado as notas

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,48 @@
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
-const note = {
-  date: new Date(),
-  content: 'Hello World'
+interface Note {
+  id: string;
+  date: Date;
+  content: string
 }
 
 export function App() {
+  const [search, setSearch] = useState('')
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const storagedNotes = localStorage.getItem('notes')
+    if (storagedNotes) {
+      return JSON.parse(storagedNotes)
+    }
+    return []
+  })
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [...notes, newNote]
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
+
+
   return (
 
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -17,6 +52,7 @@ export function App() {
           type="text"
           placeholder='Busque suas notas...'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
+          onChange={handleSearch}
         />
       </form>
 
@@ -24,8 +60,11 @@ export function App() {
 
       <div className='grid grid-cols-3 gap-4 auto-rows-[250px]'>
 
-        <NewNoteCard />
-        <NoteCard note={note} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        {filteredNotes.map(note => {
+          return <NoteCard key={note.id} note={note} />
+        })}
 
       </div>
 
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -4,7 +4,12 @@ import { ChangeEvent, FormEvent, useState } from 'react'
 import { toast } from 'sonner'
 
 
-export function NewNoteCard() {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+
+export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
   const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
   const [content, setContent] = useState('')
 
@@ -23,9 +28,12 @@ export function NewNoteCard() {
   function handleSaveNote(e: FormEvent) {
     e.preventDefault()
 
+    onNoteCreated(content)
+
+    setContent('')
+    setShouldShowOnBoarding(true)
+
     toast.success('Nota criada com sucesso!')
-
-
   }
 
   return (
@@ -59,6 +67,7 @@ export function NewNoteCard() {
                     placeholder='Escreva sua nota...'
                     className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
                     onChange={handleContentChanged}
+                    value={content}
                   />
                 )}
               </div>
